9d8b63fc56d9984dc8cc2603e567e289035a9605
dabidgmz <david.gmzherrera28@gmail.com>
2023-12-08 11:38:36 -0600

    otra vez cambios

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,13 @@ use App\Http\Controllers\DetalleSensorController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+*/
+
 Route::group([
 
     'middleware' => 'api',
