16c8c7855e5e6913c86b93f02ca66a3a66b44714
dabidgmz <david.gmzherrera28@gmail.com>
2023-12-08 11:35:23 -0600

    cambios de rutas

--- routes/api.php
+++ routes/api.php
@@ -32,15 +32,15 @@ Route::group([
 
 ], function ($router) {
 
-    
-
-});
-//rutas jwt user
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('logout', [AuthController::class, 'logout']);
 Route::post('refresh', [AuthController::class, 'refresh']);
 Route::post('me', [AuthController::class, 'me']);
+
+});
+//rutas jwt user
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
