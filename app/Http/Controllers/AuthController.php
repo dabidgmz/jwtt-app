4236743d487d007877a8e1c02afdc1dae01766d2
dabidgmz <david.gmzherrera28@gmail.com>
2023-12-10 15:22:23 -0600

    validaciones usuario o AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,31 +48,57 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        // Validar los datos del usuario
+        $messages = [
+            'name.required' => 'El nombre es obligatorio.',
+            'name.string' => 'El nombre debe ser una cadena de texto.',
+            'name.min' => 'El nombre debe tener al menos :min caracteres.',
+            'name.max' => 'El nombre no puede tener más de :max caracteres.',
+            'lastname.required' => 'El apellido es obligatorio.',
+            'lastname.string' => 'El apellido debe ser una cadena de texto.',
+            'lastname.max' => 'El apellido no puede tener más de :max caracteres.',
+            'phone.required' => 'El número de teléfono es obligatorio.',
+            'phone.string' => 'El número de teléfono debe ser una cadena de texto.',
+            'phone.regex' => 'El número de teléfono debe contener exactamente 10 dígitos y solo dígitos.',
+            'matricula.required' => 'La matrícula es obligatoria.',
+            'matricula.string' => 'La matrícula debe ser una cadena de numeros.',
+            'matricula.min' => 'La matrícula debe tener al menos :min caracteres.',
+            'matricula.max' => 'La matrícula no puede tener más de :max caracteres.',
+            'email.required' => 'El correo electrónico es obligatorio.',
+            'email.string' => 'El correo electrónico debe ser una cadena de texto.',
+            'email.email' => 'El correo electrónico debe ser una dirección de correo electrónico válida.',
+            'email.max' => 'El correo electrónico no puede tener más de :max caracteres.',
+            'email.unique' => 'El correo electrónico ya ha sido registrado.',
+            'departament.required' => 'El departamento es obligatorio.',
+            'departament.string' => 'El departamento debe ser una cadena de texto.',
+            'departament.max' => 'El departamento no puede tener más de :max caracteres.',
+            'password.required' => 'La contraseña es obligatoria.',
+            'password.string' => 'La contraseña debe ser una cadena de texto.',
+            'password.min' => 'La contraseña debe tener al menos :min caracteres.',
+            'password.confirmed' => 'La confirmación de la contraseña no coincide.',
+        ];
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|min:3|max:255',
             'lastname' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:user', // Asegúrate de ajustar el nombre de la tabla según tu configuración
+            'email' => 'required|string|email|max:255|unique:user',
             'departament' => 'required|string|max:255',
-            'phone' => 'required|string|max:255',
+            'phone' => 'required|string|regex:/^[0-9]{10}$/',
+            'matricula' => 'required|string|min:3|max:8',
             'password' => 'required|string|min:6|confirmed',
-        ]);
-    
+        ], $messages);
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 422);
         }
-    
         $user = User::create([
             'name' => $request->input('name'),
             'lastname' => $request->input('lastname'),
             'email' => $request->input('email'),
             'departament' => $request->input('departament'),
             'phone' => $request->input('phone'),
+            'matricula' => $request->input('matricula'),
             'status' => true,
             'password' => bcrypt($request->input('password')),
         ]);
         Mail::to($user->email)->send(new \App\Mail\VerifyEmail($user));
-    
         return response()->json(['msg' => "Registro correcto", 'data' => $user]);
     }
 
